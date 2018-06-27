00e850cc1f7ad86686ffa9b7c8c0388a1d15b5b9
Unknown <Kim.swope@yahoo.com>
2018-06-27 08:49:15 -0400

    Moved Navbar from test front end site/working on current site

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,10 +1,64 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
-const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+/* This is code that was original to 20.11 Book App or Project 3 Front End Boilerplate */
+
+/*const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-primary">
     <a className="navbar-brand" href="/">
       React Reading List
     </a>
+  </nav>
+);
+*/
+
+const Nav = props => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to="/">
+      FitLife
+    </Link>
+    <div>
+      <ul className="navbar-nav">
+        <li
+          className={
+            window.location.pathname === "/" ||
+            window.location.pathname === "/about"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/" className="nav-link">
+            About
+          </Link>
+        </li>
+
+        <li
+          className={
+            window.location.pathname === "/discover"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/discover" className="nav-link">
+            Discover
+          </Link>
+        </li>
+
+        <li
+          className={
+            window.location.pathname === "/search"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/search" className="nav-link">
+            Search
+          </Link>
+        </li>
+        
+      </ul>
+    </div>
   </nav>
 );
 
