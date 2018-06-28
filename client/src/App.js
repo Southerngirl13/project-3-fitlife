af0971f23d7572f75fdd2f71cd7e91f28ef448f0
Samantha Olvera <smolvera2010@yahoo.com>
2018-06-27 21:13:03 -0400

    Sam's initial commit working home page
    working home.js, signin, signup, and app.js files changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 import Nav from "./components/Nav";
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <Home />
+            </div>
+          )} />
+          <Route exact={true} path='/signin' render={() => (
+            <div className="App">
+              <SignIn />
+            </div>
+          )} />
+          <Route exact={true} path='/signup' render={() => (
+            <div className="App">
+              <SignUp />
+            </div>
+          )} />
+
+        </div>
+
+      </Router>
+    )
+  }
+};
 
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,8 +3,16 @@ import React from "react";
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
-      React Reading List
+      Fit Life
     </a>
+    <ol class="breadcrumb">
+      <li><a href="/">Home</a></li>
+      {/* <li><a href="/">Private</a></li>
+    <li><a href="/">Pictures</a></li>
+    <li class="active">Vacation</li>         */}
+    </ol>
+    <a href="signin">Sign In</a>
+    <a href="signup" >Sign Up</a>
   </nav>
 );
 
