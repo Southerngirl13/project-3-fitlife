05d5d19e79981495c4ac90e392d5476deacc92f2
Unknown <Kim.swope@yahoo.com>
2018-06-28 02:21:22 -0400

    Auto stash before merge of "master" and "origin/master"
    Changed Pupster to FitLife on Title to see if it pushes to Heroku

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,7 @@ import Col from "../components/Col";
 const About = () => (
   <div>
     <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
-      <h1>Pupster</h1>
+      <h1>FitLife</h1>
       <h2>They're the Good Boys and Girls</h2>
     </Hero>
     <Container style={{ marginTop: 30 }}>
