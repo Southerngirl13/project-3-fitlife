ba34ddbfe55161fc351edba660e574b32c529ac6
Unknown <Kim.swope@yahoo.com>
2018-07-02 20:29:35 -0400

    Add C's files for login

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import standby2 from '../images/standby2.jpg'
 import "./Blog.css";
 
-
 class Blog extends Component {
   render() {
     return (
       <div>
         <div className="Blog section flex-container">
-          <p className='section-paragraph'></p>
           <div className="item">
             <img src={standby2} className="thumb" />
             <div className="blog-overlay">
@@ -29,6 +27,26 @@ class Blog extends Component {
             </div>
           </div>
 
+ <div className="item">
+            <img src={standby2} className="thumb" />
+            <div className="blog-overlay">
+              <div className="text">
+                <h3>Dashboard</h3>
+                <p><a href="https://southerngirl13.github.io/kjs-portfolio/index.html" target="_blank" rel="noopener noreferrer">This would be a Section <i className="fa fa-external-link"></i></a></p>
+              </div>
+            </div>
+          </div>
+
+           <div className="item">
+            <img src={standby2} className="thumb" />
+            <div className="blog-overlay">
+              <div className="text">
+                <h3>Dashboard</h3>
+                <p><a href="https://southerngirl13.github.io/kjs-portfolio/index.html" target="_blank" rel="noopener noreferrer">This would be a Section <i className="fa fa-external-link"></i></a></p>
+              </div>
+            </div>
+          </div>
+
           <div className="item">
             <img src={standby2} className="thumb" />
             <div className="blog-overlay">
@@ -44,6 +62,7 @@ class Blog extends Component {
             <div className="blog-overlay">
               <div className="text">
                 <h3>Panels</h3>
+                <p> Put copy here</p>
                 <p><a href="https://southerngirl13.github.io/kjs-portfolio/index.html" target="_blank" rel="noopener noreferrer">This would be a feature  <i className="fa fa-external-link"></i></a></p>
               </div>
             </div>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,7 +5,6 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 import logo from "./logo.png";
 import Blog from "../components/Blog";
-import Card from "../components/Card";
 
 
 const About = () => (
@@ -24,38 +23,14 @@ const About = () => (
       <Row>
         <Col size="md-12">
         <p>
-          Create excellence. Easily.
-          Monitor, Understand, Optimize
-          Optimize key aspects of your athletes’ preparation 
-          and keep them fit, healthy, happy, and ready to win. Cost-effectively. From youth to pro.
-          Engage Everyone blah blah
-          Get the whole team connected as you increase collaboration, communication and compliance for the greater good.
-          Gain insight
-          Enjoy a dashboard view of everything that counts, featuring evidence-based stats, analysis, alerts, graphs, reports, recommendations, and more.
-          Identify issues
-          Identify tired and at-risk athletes in real-time and make adequate workload adjustments to minimize the risk of injury and illness.
-          Get results
-          Keep everyone healthy, working their hardest, happy and motivated, ready to perform at their best.
-############
-         
-          Build winners safely
-          Make better coaching decisions by using a holistic approach
-          to workload management and injury prevention.  blah blah
-
-          Centralize everything
-Collect and manage athlete data easily, instantly and remotely. Everyone stores and retrieves their data in the same accessible place.
-
-Stay informed and focused
-Take advantage of automated analysis, alerts, recommendations & reports to motivate, inform, prevent issues and promote excellence at all levels.
-         
-          Streamline record keeping
-Say goodbye to notepads & spreadsheets as you manage athlete data & records online in seconds. Store athlete data, docs, videos, medical records, testing results and more.
-Save money
-With the AthleteMonitoring.com modular structure you pay only for the features you need, so you can keep your performance program as cost-effective as possible.
-          </p>
+          Create excellence. Easily.   Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah 
+          blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah 
+          Blah blah blah Blah blah blah Blah blah blah v
+    
+ </p>
         </Col>
       </Row>
-      <Row>
+     {/*} <Row>
       <Col size="md-6">
         <p> This is App Feature #1</p>
         </Col>
@@ -79,8 +54,13 @@ With the AthleteMonitoring.com modular structure you pay only for the features y
         <p>This is App Feature #6</p>
         </Col>
         </Row>
+        */}
+        <Row>
+        <Col size="md-12">
+          <h1>Fit Life Features</h1>
+        </Col>
+</Row> 
         <Blog />
-        <Card />
     </Container>
   </div>
 );
