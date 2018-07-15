d302852a4eadbb6bf7db5a5eda3a351d80800b80
Samantha Olvera <smolvera2010@yahoo.com>
2018-07-15 16:04:49 -0400

    merge conflicts resolved- Sam

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
 import AssessmentForm from "./pages/AssessmentForm";
-import KJS from "./pages/KJS";
-=======
->>>>>>> sam
 import Shop from "./pages/Shop";
+import Blog from "./components/Blog"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
