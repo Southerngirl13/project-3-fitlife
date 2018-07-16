d89e972a494929dd83d9b559de809ae4afc1bb56
Unknown <Kim.swope@yahoo.com>
2018-07-16 15:55:16 -0400

    Done and Done. Peace out FitLife

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -20,23 +20,23 @@ const Contact = () => (
       <h2 className="shoptitle">The FitLife Project</h2>
     </Hero>
     <Container style={{ marginTop: 40 }}>
-      <Row className="text-center22"><h1>Objectives</h1> </Row>
+      {/* <Row className="text-center22"><h1>Objectives</h1> </Row>
       <Row>
         <Col className="team" size="md-12">
-          
+{/*           
           <p>
             Baseline Photos/videos: ie before/after photo and user uploaded movement video (for example them performing an exercise movement)
             Goals/objectives: For example, “lose 20lbs before my wedding” or “Improve bench press to 225lbs”.
             Key Performance Indicators/ Tests/ Assessment: These are key [quantitative] measurables related to the goals. For example, if the goal is to improve bench press, the biweekly bench press test will be recorded here. Similarly it could be weight, bodyfat, etc.
-            Workout History/Journal: This is a list of completed workouts with the date/time</p>     
-        </Col>
-      </Row>
-     
-      <Row>
+            Workout History/Journal: This is a list of completed workouts with the date/time</p>      */}
+        {/* </Col>
+      </Row>  */}
+      <h1 className="flfeatures">Our Team</h1>
+      {/* <Row>
         <Col size="md-12">
           <h1 className="ourTeam">Our Team</h1>
-        </Col>
-</Row>
+        </Col> */}
+{/* </Row> */}
 <Row>
         <Col id="teamcol" size="md-4">
           <img className="memberimg" src={cece} alt={"cece"} style={{ height: 120, flex: 1, width: undefined, padding: 0 }} />
@@ -67,20 +67,19 @@ const Contact = () => (
         <Col id="teamcol" size="md-4">
           <img className="memberimg" src={jamar} alt={"jamar"} style={{ height: 120, flex: 1, width: undefined, padding: 0 }} />
           <p className="memname">Jamar Parham</p>
-          <div className="membercopy"><p> <li>React Video Component</li><li> Page</li> </p></div>
+          <div className="membercopy"><p> <li>React Video Component</li><br /></p></div>
           <hr className="memberhr"/>
         </Col>
        <Col id="teamcol" size="md-4">
           <img className="memberimg" src={logo} alt={"logo"} style={{ height: 120, flex: 1, width: undefined, padding: 0 }} />
           <p className="memname">Patrick Francis</p>
-          <div className="membercopy"><p> <li>TBD</li><li>TBD</li> </p></div>
+          <div className="membercopy"><p> <li>Work Out Table</li><br /></p></div>
           <hr className="memberhr"/>
         </Col>      
       </Row>
-      <Row>
+      {/* <Row>
       <p className="specialthanks">Special thanks to the following instructors and TA's for all their help:</p>
-
-          </Row>
+          </Row> */}
     </Container>
   </div>
 );
